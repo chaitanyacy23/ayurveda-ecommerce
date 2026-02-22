11be8c4ed07015c2dc5e363091f08ef0a777b376
chaitanyacy23 <chaitanyacy23@gmail.com>
2026-02-22 19:50:26 +0530

    Cleaned unwanted cache and temp files

--- Ayurved.V1/obj/Debug/net8.0/Ayurved.V1.AssemblyInfo.cs
+++ Ayurved.V1/obj/Debug/net8.0/Ayurved.V1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ayurved.V1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fc48d8f857b098a181e9c7d71d21322bb19f5bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ayurved.V1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ayurved.V1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
